aacacbe84f4803fbc3f8d4966a3d20e3c8e1a8a3
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-11-24 17:59:06 -0600

    localized w3.css into public folder

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-
 import './App.css';
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
+
 
 class Search extends Component {
 
