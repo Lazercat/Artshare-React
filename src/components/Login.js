b287fdfec62572d2e99c9a75ac0747e8878377d9
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-11-27 12:04:50 -0600

    rolling back fb login functionality for now

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,19 +12,7 @@ class Login extends React.Component {
   render() {
     return (
     <div>
-      <div class="fb-login-button"
-              appId={process.env.FACEBOOK_APP_ID}
-              autoLoad={true}
-              fields="name,email,picture"
-             data-max-rows="1"
-             data-size="medium"
-             data-button-type="login_with"
-             data-show-faces="true"
-             data-auto-logout-link="true"
-             data-use-continue-as="false"
-              classNames="btn-facebook"
-              id= "btn-social-login">
-        </div>
+      <h1>Login could happen here</h1>
     </div>
     )
   }
