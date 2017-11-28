3902dbdde21b3e2666e4c130f5eefe936e3a247a
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-11-28 00:11:39 -0600

    style updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
          <img src="/artshare.png" className="App-logo" alt="logo" />
          <h1 className="App-title">Welcome to Artshare: Share your art online!</h1>
        </header>
-       <div className="Navbar">
+       <div className="navbar-xs">
             <nav className="navbar navbar-inverse">
               <div className="container-fluid">
                 <div className="navbar-header">
