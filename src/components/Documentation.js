178f1be9cadfe04e65d10bb7461f85fee6d72bac
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-11-28 11:18:47 -0600

    minor change to documentation

--- src/components/Documentation.js
+++ src/components/Documentation.js
@@ -15,9 +15,10 @@ class Documentation extends Component {
             <h3>MVP Features </h3>
             <p>This web application was built and completed in 7 days with the following features.</p>
             <ul>
-              <li>A back-end ArtShare Api that stores all data <a href="https://artshare-api.herokuapp.com">https://artshare-api.herokuapp.com</a></li>
+
+              <li>A back-end ArtShare Api running on Heroku that stores all data <a href="https://artshare-api.herokuapp.com">https://artshare-api.herokuapp.com</a></li>
               <li>A Cloudinary API that handles and stores all image files</li>
-              <li>A front-end web application written in React.js (this website)</li>
+              <li>A front-end web application running on Heroku written in React.js (this website)</li>
               <li>Users can upload art to Cloudinary and post data to MongoDB configured via my Artshare API Node.js, Express Web App</li>
               <li>Public can view all posts that have been shared</li>
               <li>A link to my website, <a href="http://www.jesselewis.work">www.jesselewis.work</a> where you can learn more about me, my work, or even hire me</li>
